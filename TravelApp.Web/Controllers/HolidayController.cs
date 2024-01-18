dab63a1cabd2592f9e607a8539f45ab3d394532c
Stanil007 <stanil107@gmail.com>
2024-01-18 17:28:42 +0000

    Created Admin Area and moved Amenity, Category controllers to it, also created Holiday controller as well for the Admin

--- TravelApp.Web/Controllers/HolidayController.cs
+++ TravelApp.Web/Controllers/HolidayController.cs
@@ -8,16 +8,9 @@ namespace TravelApp.Web.Controllers
     public class HolidayController : Controller
     {
         private readonly IHolidayService holidayService;
-        private readonly ICategoryService categoryService;
-        private readonly IAmenityService amenityService;
-
-        public HolidayController(IHolidayService _holidayService,
-                                 ICategoryService _categoryService, 
-                                 IAmenityService _amenityService)
+        public HolidayController(IHolidayService _holidayService)
         {
             holidayService = _holidayService;
-            categoryService = _categoryService;
-            amenityService = _amenityService;
         }
 
         [AllowAnonymous]
@@ -31,80 +24,6 @@ namespace TravelApp.Web.Controllers
         {
             var holiday = holidayService.GetByIdAsync(id);
             return View(holiday);
-        }
-
-        public IActionResult Create()
-        {
-            var categories = categoryService.GetAllAsync();
-            var amenities = amenityService.GetAllAsync();
-
-            ViewBag.Categories = categories;
-            ViewBag.Amenities = amenities;
-
-            return View();
-        }
-
-        [HttpPost]
-        [ValidateAntiForgeryToken]
-        public async Task<IActionResult> Create(HolidayDto holidayDto, List<int> selectedAmenities)
-        {
-            if (ModelState.IsValid)
-            {
-                await holidayService.CreateAsync(holidayDto, selectedAmenities);
-                return RedirectToAction(nameof(Index));
-            }
-
-            var categories = categoryService.GetAllAsync();
-            var amenities = amenityService.GetAllAsync();
-            ViewBag.Categories = categories;
-            ViewBag.Amenities = amenities;
-
-            return View(holidayDto);
-        }
-
-        public async Task<IActionResult> Edit(int id)
-        {
-            var holiday = await holidayService.GetByIdAsync(id);
-            var categories = await categoryService.GetAllAsync();
-            var amenities = await amenityService.GetAllAsync();
-
-            ViewBag.Categories = categories;
-            ViewBag.Amenities = amenities;
-
-            return View(holiday);
-        }
-
-        [HttpPost]
-        [ValidateAntiForgeryToken]
-        public async Task<IActionResult> Edit(HolidayDto holidayDto, List<int> selectedAmenities)
-        {
-            if (ModelState.IsValid)
-            {
-                await holidayService.UpdateAsync(holidayDto, selectedAmenities);
-                return RedirectToAction(nameof(Index));
-            }
-
-            var categories = await categoryService.GetAllAsync();
-            var amenities = await amenityService.GetAllAsync();
-
-            ViewBag.Categories = categories;
-            ViewBag.Amenities = amenities;
-
-            return View(holidayDto);
-        }
-
-        public async Task<IActionResult> Delete(int id)
-        {
-            var holiday = await holidayService.GetByIdAsync(id);
-            return View(holiday);
-        }
-
-        [HttpPost, ActionName("Delete")]
-        [ValidateAntiForgeryToken]
-        public async Task<IActionResult> DeleteConfirmed(int id)
-        {
-            await holidayService.DeleteAsync(id);
-            return RedirectToAction(nameof(Index));
         }
     }
 }
